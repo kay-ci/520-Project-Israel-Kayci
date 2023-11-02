3ec3085ed214b0782957c7410d6cc761c9fcc5d2
izzydotexe <israel.aristide@dawsoncollege.qc.ca>
2023-11-01 23:49:05 -0400

    add basic fetch in client side

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -1,7 +1,27 @@
+import { useEffect, useState } from 'react';
+
 function Card(){
+
+  const [meteors, setMeteors] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3000/meteorites').then(res => {
+      if (res.ok) {
+        return res.json();
+      } 
+
+      return Promise.reject('Could not fetch meteorites');
+    }).then(json => {
+      setMeteors(json.data.splice(0, 20).map(x => {
+        return <p>Meteor: {x.name}, mass: {x.mass}</p>;
+      }));
+    });
+  }, [setMeteors]);
+
   return(
     <div className="card-div">
       This will contain fetched data of a meteorite
+      { meteors }
     </div>
   );
 }
