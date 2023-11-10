59f3062e8675e49c289504b62120a37958b15a12
Kayci Davila <kayci.davila@dawsoncollege.qc.ca>
2023-11-10 15:35:17 -0500

    zoom (in progress)

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -1,10 +1,21 @@
-
+import CameraFlyTo, { CameraFlyToProps } from './CameraFlyTo';
 function Card({meteor}){
+  const handleClick = (e) => {
+    e.preventDefault();
+    console.log(`The link for ${meteor.name} was clicked.`);
+  };
+  const showLocation = (e) =>{
+    const viewer = document.querySelector('.viewer');
+    viewer.appendChild(
+      <CameraFlyTo {...args} destination={Cartesian3.fromDegrees(139.767052, 35.681167, 100)} />
+    );
+  };
   return(
-    <div className="card-div">
+    <div className="card-div" onClick={handleClick}>
       <p>{meteor.name}</p>
       <p>Mass: {meteor.mass}</p>
     </div>
   );
 }
+
 export default Card;
--- client/src/components/Globe.js
+++ client/src/components/Globe.js
@@ -1,14 +1,17 @@
 import { Viewer, Entity } from 'resium';
 import { Cartesian3 } from 'cesium';
 
-const position = Cartesian3.fromDegrees(-74.0707383, 40.7117244, 100);
 const pointGraphics = { pixelSize: 10 };
 
-function Globe(){
+function Globe({meteors}){
   return (
     <div className="globe-div">
       <Viewer className="viewer">
-        <Entity position={position} point={pointGraphics} />
+        {meteors.map(meteor => 
+          <Entity key = {meteor.name}
+            position = {Cartesian3.fromDegrees(-74.0707383, 40.7117244, 100)}
+            point = {pointGraphics}
+          />)}
       </Viewer>
     </div>
   );
