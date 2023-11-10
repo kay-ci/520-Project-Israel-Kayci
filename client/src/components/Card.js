a85634cd821e5de024fc4010cdb1a0cbed983c90
Kayci Davila <kayci.davila@dawsoncollege.qc.ca>
2023-11-09 23:31:39 -0500

    Added more css (need help with resium sizing

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -1,28 +1,10 @@
-import { useEffect, useState } from 'react';
 
-function Card(){
-
-  const [meteors, setMeteors] = useState([]);
-
-  useEffect(() => {
-    fetch('/meteorites').then(res => {
-      if (res.ok) {
-        return res.json();
-      } 
-
-      return Promise.reject('Could not fetch meteorites');
-    }).then(json => {
-      setMeteors(json.data.splice(0, 20).map(x => {
-        return <p>Meteor: {x.name}, mass: {x.mass}</p>;
-      }));
-    });
-  }, []);
-
+function Card({meteor}){
   return(
-    <div className="card-div">
-      This will contain fetched data of a meteorite
-      { meteors }
-    </div>
+    <section className="card-div">
+      <p>{meteor.name}</p>
+      <p>Mass: {meteor.mass}</p>
+    </section>
   );
 }
 export default Card;
--- client/src/components/Results.js
+++ client/src/components/Results.js
@@ -1,11 +1,27 @@
+import { useEffect, useState } from 'react';
 import Card from './Card';
 
 function Results(){
-  return(
-    <div className="results">
-      <Card/>
-      <div></div>
-    </div>
-  );
+  const [meteors, setMeteors] = useState([]);
+
+  useEffect(() => {
+    fetch('/meteorites').then(res => {
+      if (res.ok) {
+        return res.json();
+      } 
+
+      return Promise.reject('Could not fetch meteorites');
+    }).then(json => {
+      setMeteors(json.data.splice(0, 20));
+    });
+  }, []);
+
+  if (meteors.length > 0){
+    return(
+      <div className="results">
+        {meteors.map(meteor => <Card meteor = {meteor}/>)}
+      </div>
+    );
+  }
 }
 export default Results;
