fd76517dde02cd1960fb14e512e5eac0ab67cb04
izzydotexe <israel.aristide@dawsoncollege.qc.ca>
2023-11-10 13:59:17 -0500

    Filtering setup...
    - moved useEffect logic in results to a button in filterbar
    
    - Added filterbar

--- client/src/components/FilterBar.js
+++ client/src/components/FilterBar.js
@@ -1,8 +1,70 @@
-function FilterBar(){
+function FilterBar({ sendQuery }){
+
+  function createQuery() {
+
+    const minYear = document.getElementById('minYear');
+    const maxYear = document.getElementById('maxYear');
+    const minMass = document.getElementById('minMass');
+    const maxMass = document.getElementById('maxMass');
+
+    sendQuery({
+      minYear:minYear,
+      maxYear:maxYear, 
+      minMass:minMass, 
+      maxMass:maxMass
+    });
+
+  }
+
+  function onRangeChange(event) {
+
+    const label = document.getElementById(`${event.target.id}Label`);
+    label.innerText = event.target.value;
+    
+  }
+
   return(
     <div className="filter-bar">
-
+      <input className="search-filter" placeholder="search..."/>
+      <input 
+        type="range" 
+        onChange={onRangeChange} 
+        id="minYear"
+        min="800"
+        max="2023"
+        step="1"
+      /> 
+      <label htmlFor="minYear" id="minYearLabel">800</label>
+      <input 
+        type="range" 
+        onChange={onRangeChange} 
+        id="maxYear"
+        min="800"
+        max="2023"
+        step="1"
+      /> 
+      <label htmlFor="maxYear" id="maxYearLabel">800</label>
+      <input 
+        type="range" 
+        onChange={onRangeChange} 
+        id="minMass"
+        min="0"
+        max="1000"
+        step="1"
+      /> 
+      <label htmlFor="minMass" id="minMassLabel">0</label>
+      <input 
+        type="range" 
+        onChange={onRangeChange} 
+        id="maxMass"
+        min="0"
+        max="1000"
+        step="1"
+      /> 
+      <label htmlFor="maxMass" id="maxMassLabel">0</label>
+      <button onClick={createQuery}>Search</button>
     </div>
   );
+
 }
 export default FilterBar;
--- client/src/components/UserBar.js
+++ client/src/components/UserBar.js
@@ -1,11 +1,32 @@
 import FilterBar from './FilterBar';
 import Results from './Results'; 
 
+import {useState} from 'react';
+
 function UserBar(){
+
+  const [meteors, setMeteors] = useState([]);
+
+  function sendQuery(params) {
+
+    fetch('/meteorites').then(res => {
+      
+      if (res.ok) {
+        return res.json();
+      } 
+
+      return Promise.reject('Could not fetch meteorites');
+
+    }).then(json => {
+      setMeteors(json.data.splice(0, 20));
+    });
+
+  }
+
   return(
     <div className="user-bar">
-      <FilterBar/>
-      <Results/>
+      <FilterBar sendQuery={sendQuery}/>
+      <Results meteors={meteors}/>
     </div>
   );
 }
