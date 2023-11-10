585efc558020179467a51e723e5acc92e9dbe1f6
Kayci Davila <kayci.davila@dawsoncollege.qc.ca>
2023-11-10 15:08:07 -0500

    Test click event

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -1,10 +1,15 @@
-
 function Card({meteor}){
+  const handleClick = (e) => {
+    e.preventDefault();
+    console.log(`The link for ${meteor.name} was clicked.`);
+  };
+
   return(
-    <div className="card-div">
+    <div className="card-div" onClick={handleClick}>
       <p>{meteor.name}</p>
       <p>Mass: {meteor.mass}</p>
     </div>
   );
 }
+
 export default Card;
