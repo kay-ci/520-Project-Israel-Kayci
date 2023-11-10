4c7a66d7a0542868fd75cb8c913a1553ffcfe0ca
Kayci Davila <kayci.davila@dawsoncollege.qc.ca>
2023-11-09 20:59:50 -0500

    Add more layout components

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="app">
+      <Header/>
       <Main/>
       <Footer/>
     </div>
