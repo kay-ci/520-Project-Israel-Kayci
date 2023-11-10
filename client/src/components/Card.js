f9f2e12432865de5e3bd5e3a5766a41dd093dddc
Kayci Davila <kayci.davila@dawsoncollege.qc.ca>
2023-11-10 15:37:03 -0500

    Comment out bad code

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -1,15 +1,15 @@
-import CameraFlyTo, { CameraFlyToProps } from './CameraFlyTo';
+// import CameraFlyTo, { CameraFlyToProps } from './CameraFlyTo';
 function Card({meteor}){
   const handleClick = (e) => {
     e.preventDefault();
     console.log(`The link for ${meteor.name} was clicked.`);
   };
-  const showLocation = (e) =>{
-    const viewer = document.querySelector('.viewer');
-    viewer.appendChild(
-      <CameraFlyTo {...args} destination={Cartesian3.fromDegrees(139.767052, 35.681167, 100)} />
-    );
-  };
+  // const showLocation = (e) =>{
+  //   const viewer = document.querySelector('.viewer');
+  //   viewer.appendChild(
+  //     <CameraFlyTo {...args} destination={Cartesian3.fromDegrees(139.767052, 35.681167, 100)} />
+  //   );
+  // };
   return(
     <div className="card-div" onClick={handleClick}>
       <p>{meteor.name}</p>
