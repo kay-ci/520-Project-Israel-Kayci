0eef0645356d5beff8fd8bf2172abb9e50dcb210
izzydotexe <israel.aristide@dawsoncollege.qc.ca>
2023-11-11 13:25:37 -0500

    add year select

--- client/src/components/FilterBar.js
+++ client/src/components/FilterBar.js
@@ -1,9 +1,49 @@
+import { useDateSelect } from 'react-ymd-date-select';
+import { useState } from 'react';
+
+/**
+ * Guide followed from 
+ * https://www.whitphx.info/posts/20220511-react-ymd-date-select/
+ * @author Israel Aristide
+ * @param {{
+ *   onChange: (value:int) => string,
+ *   value: string
+ * }} props 
+ */
+function YearSelect(props) {
+
+  const dateSelect = useDateSelect(props.value, props.onChange, {
+    firstYear: 800
+  });
+
+  return (
+    <>
+      <input
+        type="date"
+        value={dateSelect.dateValue || ''}
+        onChange={dateSelect.onDateChange}
+      />
+      <label>
+        Year
+        <select value={dateSelect.yearValue} onChange={dateSelect.onYearChange}>
+          {dateSelect.yearOptions.map((yearOption) =>
+            <option key={yearOption.value} value={yearOption.value}>
+              {yearOption.label}
+            </option>
+          )}
+        </select>
+      </label>
+    </>
+  );
+}
 
 /**
  * Contains all the filtering options.
  * @author Israel Aristide
  */
 function FilterBar({ sendQuery }){
+
+  const [year, setYear] = useState('800');
 
   /**
    * Gathers the filter params input by the user and sends the query using the
@@ -43,7 +83,7 @@ function FilterBar({ sendQuery }){
   return(
     <div className="filter-bar">
       <input className="search-filter" placeholder="search..."/>
-      <input 
+      {/* <input 
         type="range" 
         onChange={onRangeChange} 
         id="minYear"
@@ -59,8 +99,10 @@ function FilterBar({ sendQuery }){
         min="800"
         max="2023"
         step="1"
-      /> 
+      />  
       <label htmlFor="maxYear" id="maxYearLabel">800</label>
+      */}
+      <YearSelect value={year} onChange={(value) => console.log(value)}/>
       <input 
         type="range" 
         onChange={onRangeChange} 
@@ -68,7 +110,7 @@ function FilterBar({ sendQuery }){
         min="0"
         max="1000"
         step="1"
-      /> 
+      />
       <label htmlFor="minMass" id="minMassLabel">0</label>
       <input 
         type="range" 
