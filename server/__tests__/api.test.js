0c5edf8177346bcca9b14a24afd3228c2fdf16dc
Kayci Davila <kayci.davila@dawsoncollege.qc.ca>
2023-11-15 18:24:04 -0500

    Mock DB for api call /meteorites/

--- server/__tests__/api.test.js
+++ server/__tests__/api.test.js
@@ -1,80 +1,29 @@
-
-const api = require('../src/routes/api.js');
+const app = require('../api.js');
 const request = require('supertest');
 const db = require('../src/db/db');
+const mockData = require('./mockData');
 
-// Mocking the DB
+/**
+ * Mocking the DB
+ * Data has to be minimum length 7 to get data back
+ */
 jest.mock('../src/db/db' );
-const mockReadAll = jest.spyOn(db.prototye, 'readAll');
+const mockReadAll = jest.spyOn(db.prototype, 'readAllCache');
+mockReadAll.mockResolvedValue(mockData);
 
-describe('Get meteorites', () => {
-  
-});
+describe('GET meteorites/', () => {
+  test('Response with no parameters', async () => {
+    const response = await request(app).get('/meteorites/');
 
-// const mockData = [{ data : [
-//   {
-//     id: '30409',
-//     name: 'Zinder',
-//     class: 'Pallasite, ungrouped',
-//     mass: '46',
-//     year: '1999',
-//     geolocation: { type: 'Point', coordinates: ['8.96667', '13.78333'] }
-//   },
-//   {
-//     id: '30410',
-//     name: 'Zlin',
-//     class: 'H4',
-//     mass: '3.3',
-//     year: '1939',
-//     geolocation: { type: 'Point', coordinates: ['17.66667', '49.25'] }
-//   },
-//   {
-//     id: '31357',
-//     name: 'Zubkovsky',
-//     class: 'L6',
-//     mass: '2167',
-//     year: '2003',
-//     geolocation: { type: 'Point', coordinates: ['41.5046', '49.78917'] }
-//   },
-//   {
-//     id: '30414',
-//     name: 'Zulu Queen',
-//     class: 'L3.7',
-//     mass: '200',
-//     year: '1976',
-//     geolocation: { type: 'Point', coordinates: ['-115.68333', '33.98333'] }
-//   }
-// ]}];
+    expect(response.status).toEqual(200);
+    expect(response.body).toEqual({
+      'status': 200,
+      'page': 1,
+      'pages':1,
+      'params': {}, 
+      'data': mockData}
+    );
+  });
 
-// beforeEach(() => {
-//   axios.get.mockReset();
-// });
-// // Mock the axios.get method
-// axios.get.mockResolvedValue({ data: mockData });
-// // app.get('meteorites', endpoint);
 
-// describe('GET /meteorites', () =>{
-//   const response = request(app).get('/meteorites?minYear=1990&maxYear=2003');
-//   expect(response.body).toEqual(
-//     {data:[{
-//       id: '30409',
-//       name: 'Zinder',
-//       class: 'Pallasite, ungrouped',
-//       mass: '46',
-//       year: '1999',
-//       geolocation: { type: 'Point', coordinates: ['8.96667', '13.78333'] }
-//     },
-//     {
-//       id: '31357',
-//       name: 'Zubkovsky',
-//       class: 'L6',
-//       mass: '2167',
-//       year: '2003',
-//       geolocation: { type: 'Point', coordinates: ['41.5046', '49.78917'] }
-//     }]}
-//   );
-//   expect(response.statusCode).toEqual(200);
-// });
-test('placeholder',  () => {
-  expect(2).toEqual(2);
 });
