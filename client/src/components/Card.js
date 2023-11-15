1f5f89119e7ded7532c426a9a4af168e8c655ec0
Kayci Nicole Davila <kayci.davila@dawsoncollege.qc.ca>
2023-11-14 19:47:25 -0500

    Make card css better

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -1,11 +1,20 @@
 function Card({meteor, handleMeteoriteZoom}){
+  const info = meteor;
+  console.log(info);
+
   return(
     <div className="card-div" onClick={handleMeteoriteZoom}>
-      <p>{meteor.name}</p>
-      <p>
-        Mass: {meteor.mass} <br/>
-        Year: {meteor.year}
-      </p>
+      <p className="meteorite-title">{meteor.name} {meteor.year}</p>
+      <hr></hr>
+      <p className="info-title"> Mass </p>
+      <p className="info">{meteor.mass} g</p>
+      <hr></hr>
+      <p className="info-title"> Class </p>
+      <p className="info">{meteor.class}</p>
+      <hr></hr>
+      <p className="info-title"> Location </p>
+      <p className="info"> 
+        {meteor.geolocation.coordinates[0]}&deg; {meteor.geolocation.coordinates[1]}&deg;</p>
     </div>
   );
 }
