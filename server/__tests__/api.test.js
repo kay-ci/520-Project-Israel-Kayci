e07424bfc8c3449eacb57ce4b0fb62aa3b74564f
Kayci Davila <kayci.davila@dawsoncollege.qc.ca>
2023-11-15 19:37:37 -0500

    Add describe blocks to organize code

--- server/__tests__/api.test.js
+++ server/__tests__/api.test.js
@@ -44,41 +44,59 @@ describe('GET meteorites/', () => {
     });
   });
 
-  test('Response with invalid year input', async () => {
-    // Invalid request since the min year is greater than max year
-    const response = await request(app).get('/meteorites/?minYear=2020&maxYear=1990');
-
-    // Invalid request since value is not a number
-    const responseNaN = await request(app).get('/meteorites/?minYear=asd&maxYear=asd');
-    
-    const ExpectedBody = {
-      'status':400,
-      'message': 'Invalid year range'
-    };
-
-    expect(response.status).toEqual(400);
-    expect(response.body).toEqual(ExpectedBody);
-
-    expect(responseNaN.status).toEqual(400);
-    expect(responseNaN.body).toEqual(ExpectedBody);
+  describe('Year query tests', () => {
+    test('Response with invalid year range', async () => {
+      const ExpectedBody = {
+        'status':400,
+        'message': 'Invalid year range'
+      };
+  
+      // Invalid request since the min year is greater than max year
+      const response = await request(app).get('/meteorites/?minYear=2020&maxYear=1990');
+  
+      expect(response.status).toEqual(400);
+      expect(response.body).toEqual(ExpectedBody);  
+    });
+  
+    test('Response with invalid year input', async () => {
+      const ExpectedBody = {
+        'status':400,
+        'message': 'Invalid year range'
+      };
+  
+      // Invalid request since value is not a number
+      const responseNaN = await request(app).get('/meteorites/?minYear=asd&maxYear=asd');
+  
+      expect(responseNaN.status).toEqual(400);
+      expect(responseNaN.body).toEqual(ExpectedBody);
+    });
   });
-
-  test('Response with invalid mass input', async () => {
-    // Invalid request since the min mass is greater than max mass
-    const response = await request(app).get('/meteorites/?minMass=2000&maxMass=10');
-
-    // Invalid request since value is not a number
-    const responseNaN = await request(app).get('/meteorites/?minMass=asd&maxMass=asdsa');
-    
-    const ExpectedBody = {
-      'status':400,
-      'message': 'Invalid mass range'
-    };
-
-    expect(response.status).toEqual(400);
-    expect(response.body).toEqual(ExpectedBody);
-
-    expect(responseNaN.status).toEqual(400);
-    expect(responseNaN.body).toEqual(ExpectedBody);
+  
+  describe('Mass query tests', () => {
+    test('Response with invalid mass range', async () => {
+      // Invalid request since the min mass is greater than max mass
+      const response = await request(app).get('/meteorites/?minMass=2000&maxMass=10');
+  
+      const ExpectedBody = {
+        'status':400,
+        'message': 'Invalid mass range'
+      };
+  
+      expect(response.status).toEqual(400);
+      expect(response.body).toEqual(ExpectedBody);
+    });
+  
+    test('Response with invalid mass input', async () => {
+      const ExpectedBody = {
+        'status':400,
+        'message': 'Invalid mass range'
+      };
+  
+      // Invalid request since value is not a number
+      const responseNaN = await request(app).get('/meteorites/?minMass=asd&maxMass=asdsa');
+  
+      expect(responseNaN.status).toEqual(400);
+      expect(responseNaN.body).toEqual(ExpectedBody);
+    });
   });
 });
